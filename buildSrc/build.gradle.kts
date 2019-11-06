71a5b8ad4f9df4f0091d66ae5edf3950c7266c0e
Studenikin Ivan <vanya.studenikin@yandex.ru>
2019-11-06 15:20:15 +0300

    fix build

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -43,7 +43,7 @@ val mixinVersion: String = properties.getProperty("mixin.version")
 
 dependencies {
     implementation(gradleApi())
-    implementation("gradle.plugin.net.minecrell:licenser:0.4.1")
+//    implementation("gradle.plugin.net.minecrell:licenser:0.4.1")
     implementation("org.ajoberstar.grgit:grgit-gradle:3.1.1")
     implementation("com.github.jengelman.gradle.plugins:shadow:5.1.0")
     implementation("net.ltgt.apt-eclipse:net.ltgt.apt-eclipse.gradle.plugin:0.21")
